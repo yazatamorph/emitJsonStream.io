ba373b9309b5690ca96c44a039d6d997a1705c91
Zareen Price <yazatamorph@gmail.com>
2021-03-07 01:47:46 -0500

    Fixed and added listener function.

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const { Readable } = require("stream");
+const JSONStream = require("JSONStream");
 const ss = require("@sap_oss/node-socketio-stream");
 
+// Can be called anywhere with access to the emitting socket instance
 const emitJsonStream = (socket, event, json) => {
-  // create source readable stream from JSON
-  const sourceStream = new Readable();
-  sourceStream.push(JSON.stringify(json));
   // create destination writable stream
   const destStream = ss.createStream();
   // emit event with stream as payload
   ss(socket).emit(event, destStream);
-  // write source stream to destination stream
-  sourceStream.pipe(destStream);
+  // stringify JSON and write to destination stream
+  destStream.write(JSON.stringify(json));
 };
 
-module.exports = emitJsonStream;
+const registerJsonStreamListener = (socket, event) => {
+  // Register the listener
+  ss(socket).on(event, (stream) => {
+    // Parses the incoming stringified JSON
+    // Refer to https://www.npmjs.com/package/JSONStream for usage details
+    const parsed = stream.pipe(JSONStream.parse("*"));
+    parsed.on("data", (data) => {
+      // Do something with your data
+    });
+  });
+};
+
+module.exports = { emitJsonStream, registerJsonStreamListener };
